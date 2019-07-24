1581b827891d5dbd23cbe2c4c77ece0930cd9d08
kiruthikaselvan <45963128+kiruthikaselvan@users.noreply.github.com>
2019-07-24 21:17:24 +0530

    Create 43.py

---
+++ 43.py
@@ -0,0 +1,12 @@
+import re
+str,ns=input(),""
+res1 =re.findall("[a-zA-Z]+",str)
+res2 =re.findall("[0-9]+",str)
+print(res1,res2)
+for i in range(len(res2)):
+    if int(res2[i])%2==0:
+        ns=ns+res1[i]*int(res2[i])
+    else:
+        ns=ns+res1[i]+res2[i]
+print(ns)
+        
